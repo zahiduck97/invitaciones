3a59a593d7b22887af2a4592b173885855ab3721
Zahid Eduardo Butron Cuenca <zahid.butron@gmail.com>
2022-09-27 20:07:58 -0500

    primera fase sin el maza ajaja

--- src/app/modulos/template1/components/misxv1/misxv1.component.ts
+++ src/app/modulos/template1/components/misxv1/misxv1.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-misxv1',
   templateUrl: './misxv1.component.html',
   styleUrls: ['./misxv1.component.scss']
 })
-export class Misxv1Component implements OnInit {
+export class Misxv1Component{
+  @ViewChild("inicio") inicio: ElementRef;
+  @ViewChild("datos") datos: ElementRef;
+  @ViewChild("lugar") lugar: ElementRef;
+  @ViewChild("fotos") fotos: ElementRef;
+  @ViewChild("personas") personas: ElementRef;
+  @ViewChild("asistencia") asistencia: ElementRef;
+  @ViewChild("contacto") contacto: ElementRef;
 
   constructor() { }
 
-  ngOnInit(): void {
+  opcion(opcion) {
+    this[opcion.valor].nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
   }
 
 }
--- src/app/modulos/template1/components/navbar/navbar.component.ts
+++ src/app/modulos/template1/components/navbar/navbar.component.ts
@@ -1,17 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, Output} from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent {
   mostrarMenu = false;
+  @Output() opcion = new EventEmitter();
+  arrayMenu= [
+    {titulo: 'INICIO', valor: "inicio"},
+    {titulo: 'DATOS', valor: "datos"},
+    {titulo: 'LUGAR', valor: "lugar"},
+    {titulo: 'FOTOS', valor: "fotos"},
+    {titulo: 'PERSONAS', valor: "personas"},
+    {titulo: 'ASISTENCIA', valor: "asistencia"},
+    {titulo: 'CONTACTO', valor: "contacto"},
+  ]
   constructor() { }
 
-  ngOnInit(): void {
-  }
   menu () {
-    this.mostrarMenu = this.mostrarMenu ? false : true;
+    this.mostrarMenu = !this.mostrarMenu;
+  }
+
+  opcionSeleccionada(valor) {
+    this.opcion.emit({valor})
+    this.menu();
   }
 }
