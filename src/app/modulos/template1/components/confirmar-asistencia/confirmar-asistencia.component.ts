a046f1c9cc7fefaafe6d6eb7f642c11887ea6950
Alain Maza <jamaza@minsait.com>
2022-10-03 00:04:02 -0500

    cha

--- src/app/modulos/template1/components/confirmar-asistencia/confirmar-asistencia.component.ts
+++ src/app/modulos/template1/components/confirmar-asistencia/confirmar-asistencia.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { InvitadosService } from '../../../../services/invitados.service';
 
 @Component({
   selector: 'app-confirmar-asistencia',
@@ -6,10 +7,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./confirmar-asistencia.component.scss']
 })
 export class ConfirmarAsistenciaComponent implements OnInit {
-
-  constructor() { }
+  @Input() token;
+  estadoLocal = 1;
+  constructor(private invitadosSevice: InvitadosService) { }
 
   ngOnInit(): void {
+    console.log('k-----------',this.estadoLocal);
+    
   }
-
+  asistencia(estadoInvitacion){
+    console.log(estadoInvitacion);
+    console.log(this.estadoLocal);
+    
+    this.invitadosSevice.update(this.token, {estadoInvitacion}).subscribe({
+      complete: () => this.estadoLocal = estadoInvitacion
+    })
+  } 
 }
