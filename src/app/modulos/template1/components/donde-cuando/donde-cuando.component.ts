8146a203d178791efdb499605857c9b10d8b4213
ARACELY <aracely.juarez@ahorra.io>
2022-10-03 19:55:38 -0500

    Maps

--- src/app/modulos/template1/components/donde-cuando/donde-cuando.component.ts
+++ src/app/modulos/template1/components/donde-cuando/donde-cuando.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
+import $ from 'jquery';
 
 @Component({
   selector: 'app-donde-cuando',
   templateUrl: './donde-cuando.component.html',
   styleUrls: ['./donde-cuando.component.scss']
 })
-export class DondeCuandoComponent implements OnInit {
+export class DondeCuandoComponent implements AfterViewInit {
 
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit(): void {
+    const elements = $('.modal-overlay, .modal');
+
+    $('.botonMapa').click(function(){
+      elements.addClass('active');
+    });
+
+    $('.close-modal').click(function(){
+      elements.removeClass('active');
+    });
   }
 
 }
