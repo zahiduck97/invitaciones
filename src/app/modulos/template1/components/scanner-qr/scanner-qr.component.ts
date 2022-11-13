62c99764438ac632b0709418645dbd8d5df8687b
AracelyJ <juarezaracely132013@gmail.com>
2022-11-12 20:26:27 -0600

    qr

---
+++ src/app/modulos/template1/components/scanner-qr/scanner-qr.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-scanner-qr',
+  templateUrl: './scanner-qr.component.html',
+  styleUrls: ['./scanner-qr.component.scss']
+})
+export class ScannerQrComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
